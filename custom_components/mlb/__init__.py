baa21410690b2d40380e7d8a38667a0485c7f769
DexDeadly <bluebulletrl@gmail.com>
2023-06-27 00:32:02 -0400

    update async

--- custom_components/mlb/__init__.py
+++ custom_components/mlb/__init__.py
@@ -77,7 +77,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         COORDINATOR: coordinator,
     }
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
